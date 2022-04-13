7c24ffe2c7ec0194e04eb040740cfd9bbb71b0fc
Aleksei <iamviga@yandex.ru>
2022-04-13 14:10:13 +0300

    Final Commit

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,8 @@ public class Main {
 
     public static void main(String[] args) {
         LocalDate today = LocalDate.now();
-        TemporalAdjuster nextTuesday = TemporalAdjusters.next(DayOfWeek.TUESDAY);{
+        TemporalAdjuster nextTuesday = TemporalAdjusters.next(DayOfWeek.TUESDAY);
+        {
             System.out.println(today.with(nextTuesday));
         }
     }
